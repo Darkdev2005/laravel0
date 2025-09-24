6bd9177ec11acd3b909576ad72a48f73037460f7
Husniddin Normo‘minov <normominovhusniddin613@gmail.com>
2025-09-24 07:06:36 -0700

    User and role models created and done

--- database/migrations/2026_01_01_000000_create_users_table.php
+++ database/migrations/2026_01_01_000000_create_users_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name');   // 👈 bo'lishi kerak
+            $table->foreignId('role_id')->default(value:2)->constrained();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
